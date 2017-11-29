bdbb07c4602c4c32b2a78dec08c08ba9f51b3c72
Andrei <tolkachev75@gmail.com>
2017-11-29 20:20:01 +0400

    Update MailTest.java

--- src/test/java/MailTest.java
+++ src/test/java/MailTest.java
@@ -31,7 +31,7 @@ public class MailTest {
     @Test(dataProvider = "testData")
     public void testSendMail(User user, String recipient, String theme) throws IOException {
         MainPage mainPage = open("https:/mail.ru", MainPage.class);
-        InboxPage inboxPage = mainPage.authorization(user).sendBtnClick().sendingMsg(recipient, theme);
+        InboxPage inboxPage = mainPage.authorization(user).sendBtnClick().sendingMsgFromTxtFile(recipient, theme);
         inboxPage.result().shouldHave(text("Ваше письмо отправлено. Перейти во Входящие"));
         inboxPage.logOut();
     }
